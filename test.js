781d2d9c5f6c3f120d168a08f17631e5b04106e9
Sharma <A.Sharma@in.imshealth.com>
2019-10-15 19:27:53 +0530

    Initial commit

---
+++ test.js
@@ -0,0 +1,14 @@
+let a = [1,2,4,5,7,3,7,88,33];
+
+for (let i =0 ;i<=a.length-1;i++){
+    
+    for (let j = a[i]+1 ; j = a[i]; j++){
+
+        if (a[i] == a[j]){
+
+
+            console.log(a[i])
+;        }
+    }
+}
+
